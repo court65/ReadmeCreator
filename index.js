64edfd2d8f438e6a1f7161ec4ad92d8dfff58deb
court65 <court_re@yahoo.com>
2022-01-14 18:14:29 -0600

    updating index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const fs=require("fs");
+const path=require("path");
+const inquirer=require("inquirer");
+const generateMarkdown=require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "contributor",
+        message: "Who created this project?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(userAnswers =>{
+        writeToFile("readme.md", generateMarkdown({
+            ...userAnswers
+        }))
+    })
+}
 
 // Function call to initialize app
 init();
